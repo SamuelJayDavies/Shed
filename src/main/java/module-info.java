3a838f48a342055818300d8e829ae39abc850442
SamuelJayDavies <samj.davies@virginmedia.com>
2023-06-27 10:58:41 +0100

    Created the JavaFX project and set up libraries

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.learningjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+
+    opens com.example.learningjavafx to javafx.fxml;
+    exports com.example.learningjavafx;
+}
