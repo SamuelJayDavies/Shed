45845fbb08120fb6b2ae3a059282aa3b9d898566
SamuelJayDavies <samj.davies@virginmedia.com>
2023-06-27 17:14:00 +0100

    Screw this

--- src/main/java/com/example/learningjavafx/HelloApplication.java
+++ src/main/java/com/example/learningjavafx/HelloApplication.java
@@ -3,21 +3,34 @@ package com.example.learningjavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class HelloApplication extends Application {
-    @Override
+    @Override // Needs to be overridden as Application has this as an abstract method
     public void start(Stage stage) throws IOException {
+
+        // Here fxmlLoader is a root node, each scene needs a root note to be passed in to act as a layout manager
+        // of sorts. Won't delve into this too much here.
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+        Scene scene = new Scene(fxmlLoader.load()); // One hierarchy lower than a stage
+
+        Image icon = new Image("IconCards.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("Shed: Login");
+
+        stage.setScene(scene); // Add scene to the stage
+        stage.show(); // Shows the stage
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
