d49cb199227a846347bc5dec9de5041643b334dc
SamuelJayDavies <samj.davies@virginmedia.com>
2023-07-03 15:58:42 +0100

    Created complex deck generation and resizes to the amount of cards, gameplay can begin

--- src/main/java/com/example/learningjavafx/ShedController.java
+++ src/main/java/com/example/learningjavafx/ShedController.java
@@ -1,6 +1,8 @@
 package com.example.learningjavafx;
 
 import higherOrLower.Card;
+import higherOrLower.Deck;
+import higherOrLower.DeckType;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -18,41 +20,53 @@ import java.util.ArrayList;
 public class ShedController {
 
     @FXML
-    private Pane testHandPane;
+    private Pane generalHandPane;
+
+    private Deck deck;
 
     @FXML
     private void initialize() {
-        testHandPane.getChildren().addAll(getCards());
+        this.deck = new Deck(DeckType.DRAW);
+        this.deck.shuffle();
+        ArrayList<Card> cards = new ArrayList<>();
+        for(int i=0; i<14; i++) {
+            cards.add(deck.deal());
+        }
+        generalHandPane.getChildren().addAll(getCards(cards));
     }
 
-    private ArrayList<ImageView> getCards() {
+    private ArrayList<ImageView> getCards(ArrayList<Card> cards) {
         ArrayList<ImageView> list = new ArrayList<>();
-        for(int i=0; i<3; i++) {
-            try {
-                FileInputStream stream = new FileInputStream("src\\main\\java\\higherOrLower\\cards\\back_of_card.png");
-                ImageView view = new ImageView(new Image(stream));
-                view.setFitWidth(100);
-                view.setFitHeight(150);
-                view.setX(i*20);
-                view.setY(10);
-                view.setOnMouseEntered(new EventHandler<MouseEvent>() {
+        int i = 14;
+        for(int j=0; j<i; j++) {
+            ImageView cardImg = new ImageView(cards.get(j).getImage());
+            cardImg.setFitWidth(100);
+            cardImg.setFitHeight(150);
+            int cardOffset = 18;
+            double middleOfWindow = generalHandPane.getPrefWidth()/2;
+            int totalCardOffset = (i-1) * cardOffset;
+            cardImg.setX(middleOfWindow - (((cardImg.getFitWidth()) + totalCardOffset) /2) + (j*cardOffset));
+            cardImg.setY(10);
+            cardImg.setOnMouseEntered(new EventHandler<MouseEvent>() {
                     @Override
                     public void handle(MouseEvent mouseEvent) {
-                        view.setY(-10);
+                        cardImg.setY(-10);
                     }
                 });
 
-                view.setOnMouseExited(new EventHandler<MouseEvent>() {
+            cardImg.setOnMouseExited(new EventHandler<MouseEvent>() {
                     @Override
                     public void handle(MouseEvent mouseEvent) {
-                        view.setY(10);
+                        cardImg.setY(10);
                     }
                 });
 
-                list.add(view);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+                list.add(cardImg);
+
+                //                view.setX(windowWidth/(3*i) + j*20);
+            //                view.setFitWidth(100);
+            //                view.setFitHeight(150);
+            //                view.setY(10);
         }
         return list;
     }
